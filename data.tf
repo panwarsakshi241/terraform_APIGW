c42a12c792598004b584082f2e5df764b1da08cc
sakshi panwar <sakshipanwar1912@gmail.com>
2023-08-01 00:09:28 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,3 +1,4 @@
+/*
 data "aws_api_gateway_rest_api" "aws_rest_api" {
   name = "DeveloperAPI"
 }
@@ -5,4 +6,4 @@ data "aws_api_gateway_rest_api" "aws_rest_api" {
 data "aws_api_gateway_resource" "developer_api_resource" {
   rest_api_id = data.aws_api_gateway_rest_api.aws_rest_api.id
   path        = "/ASPENServices/rest"
-}
+}*/
